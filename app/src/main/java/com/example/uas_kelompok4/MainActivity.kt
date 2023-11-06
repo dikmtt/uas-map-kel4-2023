bbc83472b82f35c676eeea0f58b0d9756bdf26ac
dikmtt <aleron.nathaniel@student.umn.ac.id>
2023-11-06 22:54:51 +0700

    login page

--- app/src/main/java/com/example/uas_kelompok4/MainActivity.kt
+++ app/src/main/java/com/example/uas_kelompok4/MainActivity.kt
@@ -3,44 +3,210 @@ package com.example.uas_kelompok4
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardOptions
+
+import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+
 import androidx.compose.ui.tooling.preview.Preview
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import com.example.uas_kelompok4.ui.theme.UAS_Kelompok4Theme
+
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             UAS_Kelompok4Theme {
-                // A surface container using the 'background' color from the theme
+
+                val navController = rememberNavController()
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    // color = Color.White
                 ) {
-                    Greeting("Android")
+                    NavHost(
+                        navController = navController,
+                        startDestination = "MainPage"
+                    ) {
+                        composable("MainPage") {
+                            MainPage(navController)
+                        }
+                        composable("scanqr") {
+
+                            //scanqr()
+                        }
+                        composable("login") {
+
+                            //Login
+                        }
+                        composable("register") {
+
+                            //Register()
+                        }
+                    }
                 }
             }
         }
     }
 }
 
+
+//ini utamanya
+//@Preview(showBackground = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainPage(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Top
+    ) {
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = stringResource(id = R.string.Logo_area),
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    // color = Color.Black
+                )
+            )
+
+            Text(
+                text = stringResource(id = R.string.name_area),
+                style = TextStyle(
+                    fontSize = 18.sp,
+                    // color = Color.Gray
+                )
+            )
+        }
+
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        // Buttons Area
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                onClick = { navController.navigate("scanqr") }
+            ) {
+                Text("Scan QR")
+            }
+
+            Button(
+                onClick = { navController.navigate("login") }
+            ) {
+                Text("Login")
+            }
+
+            Button(
+                onClick = { navController.navigate("register") }
+            ) {
+                Text("Register")
+            }
+        }
+    }
+}
+
+@Composable
+fun Login(
+    inputEmail: String,
+    inputPass: String,
+    onInputValueChange: (String) -> Unit,
+    onButtonClick: () -> Unit
+) {
+    LazyColumn {
+        item {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(id = R.string.input_email),
+                    style = TextStyle(
+                        fontSize = 24.sp,
+                    )
+                )
+                TextField(value = inputEmail,
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text
+                    ), onValueChange = onInputValueChange)
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(
+                    text = stringResource(id = R.string.input_pass),
+                    style = TextStyle(
+                        fontSize = 24.sp,
+                    )
+                )
+                TextField(value = inputPass,
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text
+                    ), onValueChange = onInputValueChange)
+                Spacer(modifier = Modifier.height(20.dp))
+                Button(onClick = onButtonClick) {
+                    Text(text = "Submit")
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    UAS_Kelompok4Theme {
-        Greeting("Android")
-    }
+fun PreviewLogin() {
+    Login("yo", "yo", {}, {})
+}
+
+@Composable
+fun Image(painter: Any, contentDescription: Nothing?, modifier: Any) {
+
 }
