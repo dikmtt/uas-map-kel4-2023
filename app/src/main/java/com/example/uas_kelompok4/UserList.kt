2b46236e242cf15734c59136dcd5a0043237b95a
Jeffhenry <147974247+jffhnry@users.noreply.github.com>
2023-12-21 16:36:57 +0700

    startActivityForMember langsung("Member", currUser)

--- app/src/main/java/com/example/uas_kelompok4/UserList.kt
+++ app/src/main/java/com/example/uas_kelompok4/UserList.kt
@@ -1,5 +1,6 @@
 package com.example.uas_kelompok4
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -31,18 +32,31 @@ import androidx.navigation.compose.rememberNavController
 import com.example.uas_kelompok4.model.User
 import com.example.uas_kelompok4.ui.theme.Purple80
 import com.example.uas_kelompok4.ui.theme.UAS_Kelompok4Theme
+import com.google.firebase.database.*
 
-class UserList: ComponentActivity() {
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.toMutableStateList
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+
+class UserList : ComponentActivity() {
+    private val database: FirebaseDatabase =
+        FirebaseDatabase.getInstance("https://uas-kelompok-4-5e25b-default-rtdb.asia-southeast1.firebasedatabase.app/")
+    private val userRef: DatabaseReference = database.getReference("users")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             UAS_Kelompok4Theme {
-
                 val navController = rememberNavController()
 
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    // color = Color.White
                 ) {
                     NavHost(
                         navController = navController,
@@ -56,89 +70,114 @@ class UserList: ComponentActivity() {
             }
         }
     }
-}
-
-//Might make a separate class file for this
 
 
-@Composable
-fun ListPage() {
-    val listData = remember { mutableStateListOf(
-        User("1", "admin", "admin@resto.com", "admin", "Admin"),
-        User("2","Joe", "joe@mail.com", "abcdef", "Member"),
-        User("3","Bill", "bill@mail.com", "password", "Member"),
-        User("4","Steve", "steve@mail.com", "yeehaw", "Member"),
-        User("5","Ken", "ken@resto.com", "ooofff", "Staff"),
-        User("6","James", "james@resto.com", "admout", "Staff")
-    ) }
-    ListContent(listData = listData)
-}
+    @Composable
+    fun ListPage() {
+        // Use MutableState to hold the listData
+        val listDataState = remember { mutableStateListOf<User>() }
 
-@Composable
-fun ListContent(listData: SnapshotStateList<User>) {
-    LazyColumn {
-        items(listData) { item ->
-            Row (modifier=
-            Modifier.padding(vertical = 5.dp)
-                .padding(horizontal = 5.dp)
-                .background(color = Purple80)
-                .fillMaxSize()) {
-                Column (modifier=
-                Modifier.padding(horizontal = 3.dp)
-                    .padding(vertical = 3.dp)) {
-                    Text(item.name, style=MaterialTheme.typography.titleLarge)
-                    Spacer(modifier = Modifier.height(5.dp))
-                    Row {
-                        Text(item.email, style=MaterialTheme.typography.bodySmall)
-                        Spacer(modifier = Modifier.width(2.dp))
-                        Text("-", style=MaterialTheme.typography.bodySmall)
-                        Spacer(modifier = Modifier.width(2.dp))
-                        Text(item.role, style=MaterialTheme.typography.bodySmall)
+        // Fetch data from Firebase
+        LaunchedEffect(Unit) {
+            val database = FirebaseDatabase.getInstance("https://uas-kelompok-4-5e25b-default-rtdb.asia-southeast1.firebasedatabase.app/")
+            val userRef = database.getReference("users")
+
+            val eventListener = object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val users = mutableListOf<User>()
+                    for (userSnapshot in snapshot.children) {
+                        val user = userSnapshot.getValue(User::class.java)
+                        user?.let { users.add(it) }
+                    }
+                    listDataState.clear()
+                    listDataState.addAll(users)
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    // Handle error
+                }
+            }
+            userRef.addValueEventListener(eventListener)
+        }
+
+        ListContent(listData = listDataState)
+    }
+
+    @Composable
+    fun ListContent(listData: SnapshotStateList<User>) {
+        LazyColumn {
+            items(listData) { item ->
+                Row(
+                    modifier =
+                    Modifier.padding(vertical = 5.dp)
+                        .padding(horizontal = 5.dp)
+                        .background(color = Purple80)
+                        .fillMaxSize()
+                ) {
+                    Column(
+                        modifier =
+                        Modifier.padding(horizontal = 3.dp)
+                            .padding(vertical = 3.dp)
+                    ) {
+                        Text(item.name, style = MaterialTheme.typography.titleLarge)
+                        Spacer(modifier = Modifier.height(5.dp))
+                        Row {
+                            Text(item.email, style = MaterialTheme.typography.bodySmall)
+                            Spacer(modifier = Modifier.width(2.dp))
+                            Text("-", style = MaterialTheme.typography.bodySmall)
+                            Spacer(modifier = Modifier.width(2.dp))
+                            Text(item.role, style = MaterialTheme.typography.bodySmall)
+                        }
                     }
                 }
             }
         }
     }
-}
 
-@Composable
-fun ListDetail(listItem: User) {
-    Column(modifier = Modifier.padding(5.dp).fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally) {
+    @Composable
+    fun ListDetail(listItem: User) {
         Column(
-            modifier = Modifier
-                .padding(5.dp),
+            modifier = Modifier.padding(5.dp).fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(listItem.name, style = MaterialTheme.typography.headlineLarge)
-            Spacer(modifier = Modifier.height(3.dp))
-            Text(listItem.email, style = MaterialTheme.typography.bodySmall)
-            Text(listItem.role, style = MaterialTheme.typography.bodySmall)
-            Text("Joined since:", style = MaterialTheme.typography.bodySmall)
+            Column(
+                modifier = Modifier
+                    .padding(5.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(listItem.name, style = MaterialTheme.typography.headlineLarge)
+                Spacer(modifier = Modifier.height(3.dp))
+                Text(listItem.email, style = MaterialTheme.typography.bodySmall)
+                Text(listItem.role, style = MaterialTheme.typography.bodySmall)
+                Text("Joined since:", style = MaterialTheme.typography.bodySmall)
 
-        }
-        Column(
-            modifier = Modifier
-                .padding(5.dp)
-        ) {
-            Column(modifier = Modifier.background(color = Purple80)
-                .size(100.dp)
-                .padding(5.dp)) {
-                Text("Active Promos", style = MaterialTheme.typography.bodySmall)
             }
-            Text("View History", style = MaterialTheme.typography.bodySmall)
+            Column(
+                modifier = Modifier
+                    .padding(5.dp)
+            ) {
+                Column(
+                    modifier = Modifier.background(color = Purple80)
+                        .size(100.dp)
+                        .padding(5.dp)
+                ) {
+                    Text("Active Promos", style = MaterialTheme.typography.bodySmall)
+                }
+                Text("View History", style = MaterialTheme.typography.bodySmall)
+            }
         }
     }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun PreviewList() {
+        ListPage()
+    }
+
+    /*@Preview(showBackground = true)
+    @Composable
+    fun PreviewDetail() {
+        ListDetail(User("1", "Joe", "joe@mail.com", "abcdef", "Member"))
+    }*/
 }
 
-@Preview(showBackground = true)
-@Composable
-fun PreviewList() {
-    ListPage()
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewDetail() {
-    ListDetail(User("1","Joe", "joe@mail.com", "abcdef", "Member"))
-}
